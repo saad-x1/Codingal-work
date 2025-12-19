40a9ab615a47b6b8078d98cf6a11193768aae30c
saad123659 <zainabfaraz86@gmail.com>
2025-12-19 16:27:56 +0300

    tuple

---
+++ tuple.py
@@ -0,0 +1,50 @@
+my_tuple = ()
+print(my_tuple)
+
+mytuple_1 = (1, 2, 334, 5, 5, 66, 67)
+print(mytuple_1)
+
+mytuple_2 = ("ali", "address", "salary", 23, 23.4, 23.55)
+print(mytuple_2)
+
+mytuple_3 = ("saad", [1, 234, 56], [3.3, 4.4, 5.5])
+print(mytuple_3)
+
+mytuple_4 = ('c', 'o', 'd', 'i', 'n', 'g')
+print(mytuple_4[0])
+print(mytuple_4[3])
+
+nested_tuple = ("codingal", [1, 2, 3, 4, 5], (22, 33, 44))
+print("the nested tuple is",nested_tuple)
+print("sliced tuple is",mytuple_2[:4])
+print("the value of 3 in nested tuple is",nested_tuple[1][2])
+
+
+for i in (mytuple_4):
+    print("Hello",i)
+
+myset={1,2,3,4,5,6,7,8,9}
+print(myset)
+myset2={1,2,2,2,2,2,3,4,5}
+print(myset2)
+myset2.add(6)
+print(myset2)
+myset2.add(7)   
+print(myset2)
+myset3={6,7,8,9}
+print(myset3)
+myset4={1,2,3,4}
+print(myset4.difference(myset3))
+print(myset4.symmetrprint(setc1_difference(myset3))
+
+setc1={"green","blue"}
+setc2={"blue","yellow"}
+print("original set")
+print(setc1)
+print(setc2)
+setc3=setc1.union(setc2)
+print("union of setc1 and setc2")
+print(setc3)
+setc4=setc1.intersection(setc2)
+print("intersection of setc1 and setc2")
+print(sect4)
